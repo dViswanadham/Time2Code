26dd9438bbc6fee406f5b1b412d60959abdeaace
Dheeraj Viswanadham <z5204820@unsw.edu.au>
2018-04-08 15:15:33 +1000

    give on Sun  8 Apr 15:15:33 AEST 2018

--- time2code.c
+++ time2code.c
@@ -2,7 +2,7 @@
 // Assignment 1, COMP1511 18s1: Time To Code.
 //
 // This program by Dheeraj Viswanadham (z5204820) 
-// Start: 27/03/18 | Last edited: 1/04/18
+// Start: 27/03/18 | Last edited: 8/04/18
 // Version update: 28/03/18
 // Version 1.0.2: Add version numbers and header comment. 
 // Version 1.0.1: Fix day/time variable mix-up in main function.
@@ -18,7 +18,7 @@
 #define TOWN_BROKEN_HILL    2  // ACDT
 #define TOWN_CANBERRA       3  // AEDT
 #define TOWN_DARWIN         4  // Daylight savings not observed i.e. only ACST
-#define TOWN_EUCLA          5  // Daylight savings not boserved i.e. only ACWST
+#define TOWN_EUCLA          5  // Daylight savings not observed i.e. only ACWST
 #define TOWN_HOBART         6  // AEDT
 #define TOWN_LORD_HOWE_IS   7  // LHDT
 #define TOWN_MELBOURNE      8  // AEDT
@@ -31,9 +31,9 @@
 #define TOWN_CHRISTCHURCH   12  // NZDT
 #define TOWN_WELLINGTON     13  // NZDT
 
-// Australia- Daylight savings starts 1/10/17 (October) at 2am i.e. 158, 159, 300
-//            and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100. 
-//            It then starts again on 7/10/18 at 2am finishing on 7/04/19 at 2am.             
+// Australia Daylight savings starts 1/10/17 (October) at 2am i.e. 158, 159, 300
+//         and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100.
+//         It then starts again on 7/10/18 at 2am finishing on 7/04/19 at 2am.           
 // I.e. When local daylight time was about to reach
 // Sunday, 1 April 2018, 3:00:00 am clocks were turned backward 1 hour to 
 // Sunday, 1 April 2018, 2:00:00 am local standard time instead.
@@ -51,9 +51,9 @@
 #define TIMEZONE_LHST_OFFSET 1030 // Lord Howe Standard Time
 #define TIMEZONE_LHDT_OFFSET 1100 // Lord Howe Daylight Time i.e. +30mins
 
-// New Zealand Daylight savings starts 24/09/17 (October) at 2am i.e. 158, 159, 300
-//            and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100. 
-//            It then starts again on 30/09/18 at 2am finishing on 7/04/19 at 2am.
+// New Zealand Daylight savings starts 24/09/17 (October) at 2am- 158, 159, 300
+//         and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100. 
+//         It then starts again on 30/09/18 at 2am finishing on 7/04/19 at 2am.
 // I.e. When local daylight time was about to reach
 // Sunday, 1 April 2018, 3:00:00 am clocks were turned backward 1 hour to 
 // Sunday, 1 April 2018, 2:00:00 am local standard time instead.
@@ -66,8 +66,16 @@
 
 
 // ADD YOUR #defines (if any) here
-
 #define N_TOWN 14
+
+#define UTC_ACST_START 1630 // MAR 31
+#define UTC_ACDT_START 1630 	// OCT 6 
+#define UTC_AEST_START 1600 // MAR 31
+#define UTC_AEDT_START 1600 	// OCT 6 
+#define UTC_LHST_START 1500 // MAR 31
+#define UTC_LHDT_START 1530 	// OCT 6 
+#define UTC_NZST_START 1400 // MAR 31
+#define UTC_NZDT_START 1400 	// SEP 29
 
 #define JAN 1
 #define FEB 2
@@ -92,6 +100,7 @@
 #define HOUR 100
 #define DAY 2400
 #define MAX_MINUTE 59
+#define INVALID_HOUR 24
 
 
 
@@ -101,10 +110,11 @@ void run_unit_tests(void);
 
 
 // ADD PROTOTYPES FOR YOUR FUNCTIONS HERE
-int call_town(void);
-int call_utc_month_day(void);
-int calc_utc_time(void);
-int calc_local_time(void);
+int call_town(int town);
+int call_utc_month(int utc_month);
+int call_utc_day(int utc_day);
+int calc_utc_time(int utc_time);
+int calc_local_time(int local_time);
 
 
 // DO NOT CHANGE THIS FUNCTION
@@ -190,7 +200,10 @@ int main(void) {
 //
 
 int get_local_time(int town, int utc_month, int utc_day, int utc_time) {
-    // CHANGE THIS FUNCTION
+    int time = 0;
+    int local_time = calc_local_time(time);
+	
+	// CHANGE THIS FUNCTION
     // YOU ARE NOT PERMITTED TO USE ARRAYS, LOOPS, PRINTF OR SCANF
     // SEE THE ASSIGNMENT SPECIFICATION FOR MORE RESTRICTIONS
 	
@@ -204,130 +217,287 @@ int get_local_time(int town, int utc_month, int utc_day, int utc_time) {
 // ADD YOUR FUNCTIONS HERE
 
 // Testing for INVALID_INPUT within the functions:
-int call_town(void) {
-	int town;
+int call_town(int town) {
 	
 	if (town < 0 || town >= N_TOWN) {
-	    return INVALID_INPUT;
+	    town == INVALID_INPUT;
 	}
+	return town;
 }
 
-int call_utc_month_day(void) {
-	int utc_month;
-	int utc_day;
+int call_utc_month(int utc_month) {
 	
-	if (utc_month == JAN || utc_month == MAR || utc_month == MAY || 
+	if (utc_month < JAN || utc_month > DEC) {
+		utc_month == INVALID_INPUT;
+	}
+	return utc_month;
+}
+
+int call_utc_day(int utc_day) {
+    int month = 0;
+	int utc_month = call_utc_month(month);
+	
+		if (utc_month == JAN || utc_month == MAR || utc_month == MAY || 
 		utc_month == JUL || utc_month == AUG || utc_month == OCT || 
 		utc_month == DEC) {
 		if (utc_day < MIN_DAYS || utc_day > MAX_DAYS_GENERAL) {
-			return INVALID_INPUT;
+			utc_day == INVALID_INPUT;
 		}
 	}
 	else if (utc_month == FEB) {
 		if (utc_day < MIN_DAYS || utc_day > MAX_DAYS_FEB) {
-			return INVALID_INPUT;
+			utc_day == INVALID_INPUT;
 		}
 	}
 	else if (utc_month == APR || utc_month == JUN || utc_month == SEP || 
 			utc_month == NOV) {
 		if (utc_day < MIN_DAYS || utc_day > MAX_DAYS_APR_JUN_SEP_NOV) {
-			return INVALID_INPUT;
+			utc_day == INVALID_INPUT;
 		}
 	}
+	return utc_day;
 }
 
-int calc_utc_time(void) {
-	int utc_time;
+int calc_utc_time(int utc_time) {
 	int utc_hour = utc_time/HOUR;
 	int utc_minute = utc_time%HOUR;
 	
-	if (utc_time < MIN_TIME || utc_time > MAX_TIME || utc_minute > MAX_MINUTE) {
-		return INVALID_INPUT;
+	if (utc_time < MIN_TIME || utc_time > MAX_TIME || utc_minute > MAX_MINUTE ||
+		utc_hour >= INVALID_HOUR) {
+			
+		utc_time == INVALID_INPUT;
 	}
-	else {
-		return utc_time;
-	}
+	return utc_time;
 }
 
-#define TOWN_ADELAIDE       0  // ACDT 
-#define TOWN_BRISBANE       1  // Daylight savings not observed i.e. only AEST
-#define TOWN_BROKEN_HILL    2  // ACDT
-#define TOWN_CANBERRA       3  // AEDT
-#define TOWN_DARWIN         4  // Daylight savings not observed i.e. only ACST
-#define TOWN_EUCLA          5  // Daylight savings not boserved i.e. only ACWST
-#define TOWN_HOBART         6  // AEDT
-#define TOWN_LORD_HOWE_IS   7  // LHDT
-#define TOWN_MELBOURNE      8  // AEDT
-#define TOWN_PERTH          9  // Daylight savings not observed i.e. AWST
-#define TOWN_SYDNEY         10  // AEDT
+int calc_local_time(int local_time) {
+    int month = 0, day = 0, time = 0, utc_town = 0;
+	int town = call_town(utc_town);
+	int utc_month = call_utc_month(month);
+	int utc_day = call_utc_day(day);
+	int utc_time = calc_utc_time(time);
 
-// New Zealand
-
-#define TOWN_AUCKLAND       11  // NZDT
-#define TOWN_CHRISTCHURCH   12  // NZDT
-#define TOWN_WELLINGTON     13  // NZDT
-
-// Australia- Daylight savings starts 1/10/17 (October) at 2am i.e. 158, 159, 300
-//            and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100. 
-//            It then starts again on 7/10/18 at 2am finishing on 7/04/19 at 2am.             
-// I.e. When local daylight time was about to reach
-// Sunday, 1 April 2018, 3:00:00 am clocks were turned backward 1 hour to 
-// Sunday, 1 April 2018, 2:00:00 am local standard time instead.
-
-#define TIMEZONE_AWST_OFFSET  800 // Australian Western Standard Time
-
-#define TIMEZONE_ACWST_OFFSET 845 // Australian Central Western Standard Time
-
-#define TIMEZONE_ACST_OFFSET  930 // Australian Central Standard Time
-#define TIMEZONE_ACDT_OFFSET 1030 // Australian Central Daylight Time i.e. +1hr 
-
-#define TIMEZONE_AEST_OFFSET 1000 // Australian Eastern Standard Time
-#define TIMEZONE_AEDT_OFFSET 1100 // Australian Eastern Daylight Time i.e. +1hr
-
-#define TIMEZONE_LHST_OFFSET 1030 // Lord Howe Standard Time
-#define TIMEZONE_LHDT_OFFSET 1100 // Lord Howe Daylight Time i.e. +30mins
-
-// New Zealand Daylight savings starts 24/09/17 (October) at 2am i.e. 158, 159, 300
-//            and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100. 
-//            It then starts again on 30/09/18 at 2am finishing on 7/04/19 at 2am.
-// I.e. When local daylight time was about to reach
-// Sunday, 1 April 2018, 3:00:00 am clocks were turned backward 1 hour to 
-// Sunday, 1 April 2018, 2:00:00 am local standard time instead.
-
-#define TIMEZONE_NZST_OFFSET 1200 // NZ Standard Time
-#define TIMEZONE_NZDT_OFFSET 1300 // NZ Daylight Time i.e. +1hr
-
-int calc_local_time(void) {
-	int utc_month;
-	int utc_time = calc_utc_time();
-	
-	if ((town == TOWN_ADELAIDE) && (utc_month >= MAR && (utc_day == MAX_DAYS_GENERAL) && (utc_hour >= 17) && (utc_minute >= MIN_TIME)) { // consider edge cases
-		return utc_time + (replace this);
-	
+// Australian towns:
+	// Town = Adelaide/Broken Hill:
+	if (town == TOWN_ADELAIDE || town == TOWN_BROKEN_HILL) {
+		if (utc_month < MAR) {
+			local_time = utc_time + TIMEZONE_ACDT_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == MAR) {
+			if ((utc_day == MAX_DAYS_GENERAL) && (utc_time >= UTC_ACST_START)) { 
+				local_time = utc_time + TIMEZONE_ACST_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_ACDT_OFFSET;
+			}
+		}
+		else if ((utc_month > MAR) && (utc_month < OCT)) {
+			local_time = utc_time + TIMEZONE_ACST_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == OCT) {
+			if (utc_day == 6) {
+				if (utc_time >= UTC_ACDT_START) {
+					local_time = utc_time + TIMEZONE_ACDT_OFFSET;
+				}
+				else {
+					local_time = utc_time + TIMEZONE_ACST_OFFSET;
+				}
+			}
+			else if (utc_day > 6) {
+				local_time = utc_time + TIMEZONE_ACDT_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_ACST_OFFSET;
+			}
+		}
+		else if (utc_month > OCT) {
+			local_time = utc_time + TIMEZONE_ACDT_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
 	}
+	
+	// Town = Brisbane:
+	else if (town == TOWN_BRISBANE) {
+		if ((utc_month >= JAN) && (utc_month <= DEC)) {
+			local_time = utc_time + TIMEZONE_AEST_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
+	}
+	
+	// Town = Canberra/Hobart/Melbourne/Sydney
+	else if (town == TOWN_CANBERRA || town == TOWN_HOBART || 
+	town == TOWN_MELBOURNE || town == TOWN_SYDNEY) {
+		if (utc_month < MAR) {
+			local_time = utc_time + TIMEZONE_AEDT_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == MAR) {
+			if ((utc_day == MAX_DAYS_GENERAL) && (utc_time >= UTC_AEST_START)) { 
+				local_time = utc_time + TIMEZONE_AEST_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_AEDT_OFFSET;
+			}
+		}
+		else if ((utc_month > MAR) && (utc_month < OCT)) {
+			local_time = utc_time + TIMEZONE_AEST_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == OCT) {
+			if (utc_day == 6) {
+				if (utc_time >= UTC_AEDT_START) {
+					local_time = utc_time + TIMEZONE_AEDT_OFFSET;
+				}
+				else {
+					local_time = utc_time + TIMEZONE_AEST_OFFSET;
+				}
+			}
+			else if (utc_day > 6) {
+				local_time = utc_time + TIMEZONE_AEDT_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_AEST_OFFSET;
+			}
+		}
+		else if (utc_month > OCT) {
+			local_time = utc_time + TIMEZONE_AEDT_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
+	}
+	
+	// Town = Darwin:
+	else if (town == TOWN_DARWIN) {
+		if ((utc_month >= JAN) && (utc_month <= DEC)) {
+			local_time = utc_time + TIMEZONE_ACST_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
+	}
+	
+	// Town = Eucla:
+	else if (town == TOWN_EUCLA) {
+		if ((utc_month >= JAN) && (utc_month <= DEC)) {
+			local_time = utc_time + TIMEZONE_ACWST_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
+	}
+	
+	// Town = Lord Howe Island:
+	else if (town == TOWN_LORD_HOWE_IS) {
+		if (utc_month < MAR) {
+			local_time = utc_time + TIMEZONE_LHDT_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == MAR) {
+			if ((utc_day == MAX_DAYS_GENERAL) && (utc_time >= UTC_LHST_START)) { 
+				local_time = utc_time + TIMEZONE_LHST_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_LHDT_OFFSET;
+			}
+		}
+		else if ((utc_month > MAR) && (utc_month < OCT)) {
+			local_time = utc_time + TIMEZONE_LHST_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == OCT) {
+			if (utc_day == 6) {
+				if (utc_time >= UTC_LHDT_START) {
+					local_time = utc_time + TIMEZONE_LHDT_OFFSET;
+				}
+				else {
+					local_time = utc_time + TIMEZONE_LHST_OFFSET;
+				}
+			}
+			else if (utc_day > 6) {
+				local_time = utc_time + TIMEZONE_LHDT_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_LHST_OFFSET;
+			}
+		}
+		else if (utc_month > OCT) {
+			local_time = utc_time + TIMEZONE_LHDT_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
+	}
+	
+	// Town = Perth:
+	else if (town == TOWN_PERTH) {
+		if ((utc_month >= JAN) && (utc_month <= DEC)) {
+			local_time = utc_time + TIMEZONE_AWST_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
+	}
+	
+// New Zealand towns:
+	// Town = Auckland/Christchurch/Wellington:
+	else if (town == TOWN_AUCKLAND || town == TOWN_CHRISTCHURCH || 
+	town == TOWN_WELLINGTON) {
+		if (utc_month < MAR) {
+			local_time = utc_time + TIMEZONE_NZDT_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == MAR) {
+			if ((utc_day == MAX_DAYS_GENERAL) && (utc_time >= UTC_NZST_START)) { 
+				local_time = utc_time + TIMEZONE_NZST_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_NZDT_OFFSET;
+			}
+		}
+		else if ((utc_month > MAR) && (utc_month < SEP)) {
+			local_time = utc_time + TIMEZONE_NZST_OFFSET;
+		}
+		// consider edge cases
+		else if (utc_month == SEP) {
+			if (utc_day == 29) {
+				if (utc_time >= UTC_NZDT_START) {
+					local_time = utc_time + TIMEZONE_NZDT_OFFSET;
+				}
+				else {
+					local_time = utc_time + TIMEZONE_NZST_OFFSET;
+				}
+			}
+			else if (utc_day > 29) {
+				local_time = utc_time + TIMEZONE_NZDT_OFFSET;
+			}
+			else {
+				local_time = utc_time + TIMEZONE_NZST_OFFSET;
+			}
+		}
+		else if (utc_month > SEP) {
+			local_time = utc_time + TIMEZONE_NZDT_OFFSET;
+		}
+		else {
+			local_time = INVALID_INPUT;
+		}
+	}
+
 	else {
 		return INVALID_INPUT;
 	}
+	
+	return local_time;
 }
-	
-	
-		// Australia- Daylight savings starts 1/10/17 (October) at 2am i.e. 158, 159, 300
-		//            and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100. 
-		//            It then starts again on 7/10/18 at 2am finishing on 7/04/19 at 2am.             
-		// I.e. When local daylight time was about to reach
-		// Sunday, 1 April 2018, 3:00:00 am clocks were turned backward 1 hour to 
-		// Sunday, 1 April 2018, 2:00:00 am local standard time instead.
-		
-		// New Zealand Daylight savings starts 24/09/17 (October) at 2am i.e. 158, 159, 300
-		//            and it ends on 1/04/18 (April) at 2am i.e. 158, 159, 100. 
-		//            It then starts again on 30/09/18 at 2am finishing on 7/04/19 at 2am.
-		// I.e. When local daylight time was about to reach
-		// Sunday, 1 April 2018, 3:00:00 am clocks were turned backward 1 hour to 
-		// Sunday, 1 April 2018, 2:00:00 am local standard time instead.
-	
-	if ((utc_month == APR) && 
-	
-// ADD A COMMENT HERE EXPLAINING YOUR OVERALL TESTING STRATEGY
+
+// ADD A COMMENT HERE EXPLAINING YOUR OVERALL TESTING STRATEGY 
+
 
 // run unit tests for get_local_time
 
